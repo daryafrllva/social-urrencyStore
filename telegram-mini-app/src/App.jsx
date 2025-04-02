0d3f2f89aac5f493915c50cc02642fd628ca6d54
daryafrllva <dariyf995@gmail.com>
2025-04-02 23:13:19 +0700

    написан функционал для игры кнб

--- telegram-mini-app/src/App.jsx
+++ telegram-mini-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import RockPaperScissors from "./assets/components/game/RockPaperScissors";
 import "./styles/game.css"; // Общие стили
+import "./App.css";
 
 export default function App() {
   const [showGame, setShowGame] = useState(false); // Управление отображением игры
@@ -11,12 +12,26 @@ export default function App() {
         // Главный экран с кнопкой запуска
         <div className="home-screen">
           <h1>Добро пожаловать!</h1>
+         
           <button 
             onClick={() => setShowGame(true)}
             className="start-game-button"
           >
             Играть в "Камень-Ножницы-Бумага"
           </button>
+          <button 
+            onClick={() => setShowGame(true)}
+            className="start-game-button"
+          >
+            Играть в "Угадай"
+          </button>
+          <button 
+            onClick={() => setShowGame(true)}
+            className="start-game-button"
+          >
+            Играть в "Викторину"
+          </button>
+
         </div>
       ) : (
         // Компонент игры (передаем функцию для возврата)
