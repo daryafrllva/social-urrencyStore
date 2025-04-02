3607d9a11af3dea1e312b78d0029ff6c82dee24a
daryafrllva <dariyf995@gmail.com>
2025-04-03 01:51:10 +0700

    + игра 2, выбор за карточками + изменила app.jsx - const [currentGame, setCurrentGame] = useState(null);- для того чтобы можно было использовать больше чем одну игру

---
+++ telegram-mini-app/src/assets/components/game/GameModal.jsx
@@ -0,0 +1,35 @@
+export default function GameModal({ result, onClose }) {
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        {result === 'win' ? (
+          <>
+            <h2>Поздравляем! 🎉</h2>
+            <p>Вы нашли монетку и выиграли 10 джоинов или каких там монет!</p>
+            <div className="modal-buttons">
+              <button 
+                onClick={onClose} 
+                className="modal-button win"
+              >
+                Забрать приз
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2>Увы! 😕</h2>
+            <p>Вы не нашли монетку. Попробуйте снова!</p>
+            <div className="modal-buttons">
+              <button 
+                onClick={onClose} 
+                className="modal-button lose"
+              >
+                В главное меню
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
