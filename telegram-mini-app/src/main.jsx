ac295360279b2c38999ff6a76aa0b350bd0a4cf4
CatMusso <egor.san.2005@gmail.com>
2025-04-02 23:15:59 +0700

    верстается менюшка магазина

--- telegram-mini-app/src/main.jsx
+++ telegram-mini-app/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+// Подключаем шрифт Montserrat
+import '@fontsource/montserrat/400.css'; // Regular
+import '@fontsource/montserrat/500.css'; // Medium
+import '@fontsource/montserrat/700.css'; // Bold
+
 const rootElement = document.getElementById('root');
 
 if (!rootElement) {
