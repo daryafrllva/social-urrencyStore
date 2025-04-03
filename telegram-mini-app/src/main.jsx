46f9eca5cb80eb631a270fe74be2c0913e8021b5
CatMusso <egor.san.2005@gmail.com>
2025-04-03 12:17:34 +0700

    ало что с App.jsx

--- telegram-mini-app/src/main.jsx
+++ telegram-mini-app/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-<<<<<<< HEAD
 // Подключаем шрифт Montserrat
 import '@fontsource/montserrat/400.css'; // Regular
 import '@fontsource/montserrat/500.css'; // Medium
@@ -10,14 +9,7 @@ import '@fontsource/montserrat/700.css'; // Bold
 
 const rootElement = document.getElementById('root');
 
-if (!rootElement) {
-  throw new Error('Не найден элемент с id "root"');
-}
-
-createRoot(rootElement).render(
-=======
 createRoot(document.getElementById('root')).render(
->>>>>>> 0d3f2f89aac5f493915c50cc02642fd628ca6d54
   <StrictMode>
     <App />
   </StrictMode>,
